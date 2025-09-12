1e0abdeaa4d86a9135274cc2294aa75b5aa7d969
KlimVladMaks <vkm.blog@yandex.ru>
2025-09-12 18:24:08 +0300

    Commit 8

--- django_project_klimenkov/project_first_app/views.py
+++ django_project_klimenkov/project_first_app/views.py
@@ -1,3 +1,15 @@
+from django.http import Http404
 from django.shortcuts import render
+from .models import CarOwner
 
-# Create your views here.
+
+def owner_detail(request, owner_id):
+    """
+    Представление для отображения детальной информации о владельце автомобиля.
+    """
+    try:
+        owner = CarOwner.objects.get(pk=owner_id)
+    except CarOwner.DoesNotExist:
+        raise Http404("Владелец автомобиля не найден")
+    
+    return render(request, 'owner.html', {'owner': owner})
