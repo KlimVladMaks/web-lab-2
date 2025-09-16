89bc0368ab66157b83c5bc1347cce1c63b14b205
KlimVladMaks <vkm.blog@yandex.ru>
2025-09-16 21:59:05 +0300

    Commit 20

--- django_project_klimenkov/project_first_app/urls.py
+++ django_project_klimenkov/project_first_app/urls.py
@@ -4,11 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.root_redirect),
+
     path('owner/<int:owner_id>/', views.owner_detail, name='owner_detail'),
     path('owner/list/', views.owners_list, name='owners_list'),
-    path('car/list/', views.CarsListView.as_view(), name='cars_list'),
-    path('car/<int:car_id>/', views.CarDetailView.as_view(), name='car_detail'),
     path('owner/create/', views.create_owner, name='create_owner'),
     path('owner/edit/<int:owner_id>/', views.edit_owner, name='edit_owner'),
     path('owner/delete/<int:owner_id>/', views.delete_owner, name='delete_owner'),
+
+    path('car/list/', views.CarsListView.as_view(), name='cars_list'),
+    path('car/<int:car_id>/', views.CarDetailView.as_view(), name='car_detail'),
+    path('car/edit/<int:car_id>/', views.CarUpdateView.as_view(), name='edit_car'),
 ]
--- django_project_klimenkov/project_first_app/views.py
+++ django_project_klimenkov/project_first_app/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView
+from django.urls import reverse_lazy
 from .models import Owner, Car
 from .forms import OwnerForm
 
@@ -69,5 +70,14 @@ class CarsListView(ListView):
 class CarDetailView(DetailView):
     model = Car
     template_name = 'cars/car_detail.html'
+    context_object_name = 'car'
+    pk_url_kwarg = 'car_id'
+
+
+class CarUpdateView(UpdateView):
+    model = Car
+    template_name = 'cars/edit_car.html'
+    fields = ['license_plate', 'model', 'color']
+    success_url = reverse_lazy('cars_list')
     context_object_name = 'car'
     pk_url_kwarg = 'car_id'
